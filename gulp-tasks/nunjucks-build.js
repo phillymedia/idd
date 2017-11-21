d5e3f9d4b7163124fcd2a8811bc1cfe43d943f18
Garland <garlandpotts1@gmail.com>
2017-11-21 15:58:09 -0500

    Adding gulp templating

--- gulp-tasks/nunjucks-build.js
+++ gulp-tasks/nunjucks-build.js
@@ -1,12 +1,19 @@
-module.exports = function (gulp, plugins) {
-    return function () {
-          return gulp.src('app/pages/*.+(html)')
-      .pipe(plugins.nunjucksRender({
-           path: ['app/nj-templates']
-        }))
-        .pipe(plugins.removeCode({
-           tmp: true
-        }))
-      .pipe(gulp.dest('.tmp'))
+var data = require('gulp-data');
+
+module.exports = function(gulp, plugins) {
+    return function() {
+        return gulp.src('app/pages/*.+(html)')
+            .pipe(data(function() {
+                return {
+                    data: require('./../app/data.json')
+                }
+            }))
+            .pipe(plugins.nunjucksRender({
+                path: ['app/nj-templates']
+            }))
+            .pipe(plugins.removeCode({
+                tmp: true
+            }))
+            .pipe(gulp.dest('.tmp'))
     };
 };
--- gulp-tasks/nunjucks.js
+++ gulp-tasks/nunjucks.js
@@ -1,16 +1,23 @@
-module.exports = function (gulp, plugins) {
-    return function () {
-           return gulp.src('app/pages/*.+(html)')
-      .pipe(plugins.nunjucksRender({
-           path: ['app/nj-templates']
-        }))
-        .pipe(plugins.removeCode({
-           tmp: true,
-           build: true
-        }))
-      .pipe(gulp.dest('.tmp'))
-      .pipe(plugins.browserSync.reload({
-        stream: true
-      }))
+var data = require('gulp-data');
+
+module.exports = function(gulp, plugins) {
+    return function() {
+        return gulp.src('app/pages/*.+(html)')
+            .pipe(data(function() {
+                return {
+                    data: require('./../app/data.json')
+                }
+            }))
+            .pipe(plugins.nunjucksRender({
+                path: ['app/nj-templates']
+            }))
+            .pipe(plugins.removeCode({
+                tmp: true,
+                build: true
+            }))
+            .pipe(gulp.dest('.tmp'))
+            .pipe(plugins.browserSync.reload({
+                stream: true
+            }))
     };
 };
